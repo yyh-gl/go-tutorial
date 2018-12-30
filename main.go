f4da477cf235884bb90b36f3ed7b73566d9bd322
yyh-gl <yhonda.95.gl@gmail.com>
2018-12-30 14:16:26 +0900

    1ファイルでのAPIサーバ完成

--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import (
     "fmt"
     "log"
     "net/http"
-
     "github.com/gorilla/mux"
 )
 
 func main() {
+    const Host = "localhost:5000" // Go的慣例：定数には型を指定しない
+
     router := mux.NewRouter()
-    router.HandleFunc("/v1/users/{id:[0-9]+}", usersHandler).Methods("GET")
-    log.Fatal(http.ListenAndServe("localhost:5000", router))
+    router.HandleFunc("/users/{id:[0-9]+}", usersHandler).Methods("GET")
+    fmt.Println("Server Start >> " + Host)
+    log.Fatal(http.ListenAndServe(Host, router))
 }
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Your id is %v", vars["id"]) // Path内のid
+    fmt.Fprintf(w, "User No.%v", vars["id"])
 }
