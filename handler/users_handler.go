caf4b3d82dea784573ab87b33aab093736a2a41e
yyh-gl <yhonda.95.gl@gmail.com>
2019-01-25 01:02:11 +0900

    DB接続（作業途中）

--- handler/users_handler.go
+++ handler/users_handler.go
@@ -3,11 +3,51 @@ package handler
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"net/http"
+	"time"
 )
 
+type Model struct {
+	ID        uint `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index" json:"-"`
+}
+
+// Userを表す構造体
+type User struct {
+	Model
+	Name string
+	Age  int
+}
+
+var user User
+
 func UsersHandler(w http.ResponseWriter, r *http.Request) {
+	// DBに接続
+	db := gormConnect()
+
 	vars := mux.Vars(r)
+	// 指定した条件を元に複数のレコードを引っ張ってくる
+	selectedUser := db.First(&user, vars["id"])
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "User No.%v", vars["id"])
+	fmt.Fprintf(w, "User No.%v", selectedUser)
+}
+
+func gormConnect() *gorm.DB {
+	DBMS     := "mysql"
+	USER     := "root"
+	PASS     := ""
+	PROTOCOL := "tcp(127.0.0.1:3306)"
+	DBNAME   := "go_api_db"
+
+	CONNECT = USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME
+	db,err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+    "api-server-tutorial/handler"
     "fmt"
+    "github.com/gorilla/mux"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
     "log"
     "net/http"
-    "github.com/gorilla/mux"
-    "api-server-tutorial/handler"
 )
 
 func main() {
-    const Host = "localhost:5000" // Go的慣例：定数には型を指定しない
+    const Host = "localhost:5000"
 
     router := mux.NewRouter()
     router.HandleFunc("/users/{id:[0-9]+}", handler.UsersHandler).Methods("GET")
+
     fmt.Println("Server Start >> " + Host)
     log.Fatal(http.ListenAndServe(Host, router))
 }
